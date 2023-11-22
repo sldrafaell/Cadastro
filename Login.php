8ac57e85bca68b1f67a7bce18cbfb4e2d55ca342
sldrafaell <105649990+sldrafaell@users.noreply.github.com>
2023-11-22 17:01:41 -0300

    Add files via upload

--- Login.php
+++ Login.php
@@ -36,52 +36,50 @@
 
                     <div class="content">
                     <h1 class="title ">Login</h1>
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="email" name="txtEmail" placeholder="Email">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="password" name="txtSenha" placeholder="Password">
-                </div>
-            </div>
-            <div class="field">
-                <p class="control">
-                    <input class="input" type="text" name="txtNome" placeholder="Nome">
-                </p>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="text" name="txtCpf" placeholder="Cpf">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="text" name="txtIdade" placeholder="Idade">
-                </div>
-            </div> 
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="text" name="txtIdbicicleta" placeholder="Id da bicicleta">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <div class="select">
-                        <select name="txtPagamento">
-                            <option>Forma de pagamento</option>
-                            <option>Cartão de crédito</option>
-                            <option>Cartão de débito</option>
-                            <option>Boleto</option>
-                        </select>
-                    </div>  
-                </div>
-            </div>
-            
-            
-            <button type="submit" class="button is-primary" href="Alugar.php">Cadastrar</button>
-            <a class="button is-warning" href="listar.php">Listar dados</a>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="email" name="txtEmail" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="password" name="txtSenha" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <p class="control">
+                            <input class="input" type="text" name="txtNome" placeholder="Nome">
+                        </p>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" name="txtCpf" placeholder="Cpf">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" name="txtIdade" placeholder="Idade">
+                        </div>
+                    </div> 
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" name="txtIdbicicleta" placeholder="Id da bicicleta">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <div class="select">
+                                <select name="txtPagamento">
+                                    <option>Forma de pagamento</option>
+                                    <option>Cartão de crédito</option>
+                                    <option>Cartão de débito</option>
+                                    <option>Boleto</option>
+                                </select>
+                            </div>  
+                        </div>
+                    </div>
+                    <button type="submit" class="button is-primary" href="Alugar.php">Cadastrar</button>
+                    <a class="button is-warning" href="listar.php">Listar dados</a>
                     </div>
                 </div>
             </div>
